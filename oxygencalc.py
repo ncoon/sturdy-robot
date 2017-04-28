7c15af8b6cd873471fb79d60d598abe886cb7b13
Nicholas Coon <nicholashcoon@gmail.com>
2017-04-28 13:18:29 -0600

    changed a comment in oxygencalc.py

--- oxygencalc.py
+++ oxygencalc.py
@@ -1,9 +1,9 @@
 def Oxygen():
 	# Oxygen Tank Calculation!
 	# ((PSI - 200) * Tank Constant) / Flow Rate (Litres per minute)
-	D = 0.16   ## 350liters of 02
-	E = 0.28   ## 625liters of 02
-	M = 1.56   ## 3000liters of 02
+	D = 0.16   ## 350liters of O2
+	E = 0.28   ## 625liters of O2
+	M = 1.56   ## 3000liters of O2
 	Safe = 200
 	promptpsi = raw_input ('Enter tank PSI. ')
 	print '\n'

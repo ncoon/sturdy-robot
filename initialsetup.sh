96faa13f4920be657ded501e0d8f20fff062ff7c
Nicholas Coon <nicholashcoon@gmail.com>
2017-10-04 20:36:23 -0600

    kept appending, causing the update script to clone repo several times in a row. haha

--- initialsetup.sh
+++ initialsetup.sh
@@ -6,13 +6,13 @@
 
 touch ~/sturdyrobot.sh
 chmod u+x ~/sturdyrobot.sh
-echo '#/bin/bash' >> ~/sturdyrobot.sh
+echo '#/bin/bash' > ~/sturdyrobot.sh
 echo 'python2 ~/sturdy-robot/main.py' >> ~/sturdyrobot.sh # This runs the sturdy-robot program
 
 touch ~/update.sh
 chmod u+x ~/update.sh
 echo '#/bin/bash' >> ~/update.sh
-echo 'rm -rf sturdy-robot' >> ~/update.sh
+echo 'rm -rf sturdy-robot' > ~/update.sh
 echo 'git clone https://github.com/ncoon/sturdy-robot' >> ~/update.sh # This runs an update of the sturdy-robot code
-echo "alias update='./update.sh'" >> .../usr/etc/bash.bashrc
+echo "alias update='./update.sh'" > .../usr/etc/bash.bashrc
 echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> .../usr/etc/bash.bashrc # These lines create aliases for the scripts.

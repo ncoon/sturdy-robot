1737233303ccaa9d0c7c34d2b1c32a2852dfaff1
Nicholas Coon <nicholashcoon@gmail.com>
2017-10-04 19:54:40 -0600

    sigh

--- initialsetup.sh
+++ initialsetup.sh
@@ -4,15 +4,15 @@
 # After that it creates an "update" script and alias to easily clone the repo to obtain the newest code.
 
 mkdir ~/termux/boot/
-touch ~/.termux/boot/sturdyrobot.sh
-chmod u+x ~/.termux/boot/sturdyrobot.sh
-echo '#/bin/bash' >> ~/.termux/boot/sturdyrobot.sh
-echo 'python2 ~/sturdy-robot/main.py' >> ~/.termux/boot/sturdyrobot.sh
+touch ~/com.termux/boot/sturdyrobot.sh
+chmod u+x ~/com.termux/boot/sturdyrobot.sh
+echo '#/bin/bash' >> ~/com.termux/boot/sturdyrobot.sh
+echo 'python2 ~/sturdy-robot/main.py' >> ~/com.termux/boot/sturdyrobot.sh
 
 touch ~/update.sh
 chmod u+x ~/update.sh
 echo '#/bin/bash' >> ~/update.sh
 echo 'rm -rf sturdy-robot' >> ~/update.sh
 echo 'git clone https://github.com/ncoon/sturdy-robot' >> ~/update.sh
-echo "alias update='./update.sh'" >> ~/etc/bash.bashrc
-echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> ~/etc/bash.bashrc
+echo "alias update='./update.sh'" >> /usr/etc/bash.bashrc
+echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> /usr/etc/bash.bashrc

579c4b8da5c63f9106d99da71fcc26ebdabd9864
Nicholas Coon <nicholashcoon@gmail.com>
2017-10-04 20:07:02 -0600

    Maybe

--- initialsetup.sh
+++ initialsetup.sh
@@ -3,16 +3,16 @@
 # Creates the default directory to store termux scripts, then creates and edits a script which auto runs sturdy-robot on startup of termux.
 # After that it creates an "update" script and alias to easily clone the repo to obtain the newest code.
 
-mkdir ~/com.termux/boot/
-touch ~/com.termux/boot/sturdyrobot.sh
-chmod u+x ~/com.termux/boot/sturdyrobot.sh
-echo '#/bin/bash' >> ~/com.termux/boot/sturdyrobot.sh
-echo 'python2 ~/sturdy-robot/main.py' >> ~/com.termux/boot/sturdyrobot.sh
+mkdir ../com.termux/boot/
+touch ../com.termux/boot/sturdyrobot.sh
+chmod u+x ../com.termux/boot/sturdyrobot.sh
+echo '#/bin/bash' >> ../com.termux/boot/sturdyrobot.sh
+echo 'python2 ~/sturdy-robot/main.py' >> ..com.termux/boot/sturdyrobot.sh
 
 touch ~/update.sh
 chmod u+x ~/update.sh
 echo '#/bin/bash' >> ~/update.sh
 echo 'rm -rf sturdy-robot' >> ~/update.sh
 echo 'git clone https://github.com/ncoon/sturdy-robot' >> ~/update.sh
-echo "alias update='./update.sh'" >> /usr/etc/bash.bashrc
-echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> /usr/etc/bash.bashrc
+echo "alias update='./update.sh'" >> ../usr/etc/bash.bashrc
+echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> ../usr/etc/bash.bashrc

282ac5bcb0f252e3556bcd7e44a17cb71f068049
Nicholas Coon <nicholashcoon@gmail.com>
2017-10-04 19:29:02 -0600

    still working on initialsetup.sh

--- initialsetup.sh
+++ initialsetup.sh
@@ -2,7 +2,7 @@
 
 # Creates the default directory to store termux scripts, then creates and edits a script which auto runs sturdy-robot on startup of termux.
 # After that it creates an "update" script and alias to easily clone the repo to obtain the newest code.
-termux-chroot
+
 mkdir /.termux/boot/
 touch /.termux/boot/sturdyrobot.sh
 chmod u+x /.termux/boot/sturdyrobot.sh

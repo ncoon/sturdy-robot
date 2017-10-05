4cf8e22a295b930735b68a27bf2e724354ea8f8c
Nicholas Coon <nicholashcoon@gmail.com>
2017-10-04 20:32:32 -0600

    Using a simpler method with startup and update scripts with aliases to call them easily.

--- initialsetup.sh
+++ initialsetup.sh
@@ -1,18 +1,18 @@
 #/bin/bash
 
-# Creates the default directory to store termux scripts, then creates and edits a script which auto runs sturdy-robot on startup of termux.
-# After that it creates an "update" script and alias to easily clone the repo to obtain the newest code.
+# Creates two scripts and then aliases for easy calling of the scripts
 
-mkdir .../com.termux/boot/
-touch .../com.termux/boot/sturdyrobot.sh
-chmod u+x .../com.termux/boot/sturdyrobot.sh
-echo '#/bin/bash' >> .../com.termux/boot/sturdyrobot.sh
-echo 'python2 ~/sturdy-robot/main.py' >> ...com.termux/boot/sturdyrobot.sh
+
+
+touch ~/sturdyrobot.sh
+chmod u+x ~/sturdyrobot.sh
+echo '#/bin/bash' >> ~/sturdyrobot.sh
+echo 'python2 ~/sturdy-robot/main.py' >> ~/sturdyrobot.sh # This runs the sturdy-robot program
 
 touch ~/update.sh
 chmod u+x ~/update.sh
 echo '#/bin/bash' >> ~/update.sh
 echo 'rm -rf sturdy-robot' >> ~/update.sh
-echo 'git clone https://github.com/ncoon/sturdy-robot' >> ~/update.sh
+echo 'git clone https://github.com/ncoon/sturdy-robot' >> ~/update.sh # This runs an update of the sturdy-robot code
 echo "alias update='./update.sh'" >> .../usr/etc/bash.bashrc
-echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> .../usr/etc/bash.bashrc
+echo "alias sturdyrobot='python2 ~/sturdy-robot/main.py'" >> .../usr/etc/bash.bashrc # These lines create aliases for the scripts.

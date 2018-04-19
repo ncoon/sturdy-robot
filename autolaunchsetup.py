cd075ea81fb7ce9622d71418c8865870e215b5fa
NCoon <nicholashcoon@gmail.com>
2018-04-18 20:50:33 -0600

    HMMMMMMMMM

--- autolaunchsetup.py
+++ autolaunchsetup.py
@@ -1,14 +1,13 @@
 def autolaunchsetup(): # This moves the auto launch script to ~/.termux/boot
-	try:
-		promptuser = raw_input ('Do you want sturdyrobot to launch when termux starts? Y/n\n')
-		if str(promptuser) == 'Y':
-			os.rename("~/sturdyrobotauto.sh", "~/.termux/boot/sturdyrobotauto.sh")
-			print 'It is done.\n'
-			return
-		elif str(promptuser) == 'n':
-			print 'Did nothing.\n'
-			return
-	except:
+	promptuser = raw_input ('Do you want sturdyrobot to launch when termux starts? Y/n\n')
+	if str(promptuser) == 'Y':
+		os.rename("~/sturdyrobotauto.sh", "~/.termux/boot/sturdyrobotauto.sh")
+		print 'It is done.\n'
+		return
+	elif str(promptuser) == 'n':
+		print 'Did nothing.\n'
+		return
+	else:
 		print 'Doing nothing, to try again, re run the initialsetup.sh\n'
 		print 'Please enter either Y or n.\n'
 		return
